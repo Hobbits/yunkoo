796ed5505e173303b22377b7ad5ce92423a75662
xiaosee@ssl-mail.com <036098KIll>
2013-06-22 20:00:14 +0800

    移动API

---
+++ js/services/accelerometer.js
@@ -0,0 +1,25 @@
+app.factory('accelerometer', function ($rootScope, cordovaReady) {
+    return {
+        getCurrentAcceleration: cordovaReady(function (onSuccess, onError) {
+            navigator.accelerometer.getCurrentAcceleration(function () {
+                var that = this,
+                    args = arguments;
+
+                if (onSuccess) {
+                    $rootScope.$apply(function () {
+                        onSuccess.apply(that, args);
+                    });
+                }
+            }, function () {
+                var that = this,
+                    args = arguments;
+
+                if (onError) {
+                    $rootScope.$apply(function () {
+                        onError.apply(that, args);
+                    });
+                }
+            });
+        })
+    };
+});
---
+++ js/services/geo.js
@@ -0,0 +1,27 @@
+app.factory('geo', function ($rootScope) {
+    return {
+        get: function (onSuccess, onError, options) {
+            navigator.geolocation.getCurrentPosition(function () {
+                    var that = this,
+                        args = arguments;
+
+                    if (onSuccess) {
+                        $rootScope.$apply(function () {
+                            onSuccess.apply(that, args);
+                        });
+                    }
+                }, function () {
+                    var that = this,
+                        args = arguments;
+
+                    if (onError) {
+                        $rootScope.$apply(function () {
+                            onError.apply(that, args);
+                        });
+                    }
+                },
+                options);
+
+            }
+        }
+});
